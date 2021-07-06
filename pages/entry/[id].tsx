642e3126d546fafdad6be35bcce0056fb67e6daf
Yoza Wiratama <y_wiratama@yahoo.com>
2021-07-06 21:37:52 +0700

    update text

--- pages/entry/[id].tsx
+++ pages/entry/[id].tsx
@@ -12,7 +12,7 @@ export default function EditEntryPage() {
   if (data) {
     return (
       <>
-        <Nav title="View" />
+        <Nav title="View Entries" />
         <Container>
           <h1 className="font-bold text-3xl my-2">{data.title}</h1>
           <p>{data.content}</p>
